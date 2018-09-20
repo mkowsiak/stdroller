874926f38d6ff73e9acf972694a222c7d0dde32b
mko <michalo@pi.local>
2018-09-20 08:27:07 +0200

    Added rollover and cleaned a code a little bit

--- main.c
+++ main.c
@@ -10,10 +10,12 @@
                                   // I need suffix files because I want to make
                                   // tail -F to be happy with main log file
 
-static int use_suffix;                // should we create sufix files or not
-static unsigned long long limit;      // in case limit is not passed i will use
-                                      // I_AM_OH_SO_BIG value
-static char *file_name = NULL;        // file name for log
+/* get opts flags */
+static int use_suffix            = 0;      // should we create sufix files or not
+static unsigned long long limit  = 0;      // in case limit is not passed i will use
+                                           // I_AM_OH_SO_BIG value
+static char *file_name           = NULL;   // file name for log
+static int roll_over             = 0;      // should I start writing the log from the top?
 
 char *suffix_file_name(char *suffix, char *file_name, char suffix1st, char suffix2nd) {
   strcpy(suffix, file_name);
@@ -30,19 +32,28 @@ void display_usage() {
   printf("  stdroller -f file_name|--file=file_name [--sufix|-s] [--limit|-l] [--help|-h]\n");
   printf("                                                                               \n");
   printf("options:                                                                       \n");
+  printf("                                                                               \n");
   printf("  -f file_name                                                                 \n");
-  printf("  --file=file_name    - file name where log will be stored                     \n");
+  printf("  --file=file_name    - File name where log will be stored                     \n");
+  printf("                                                                               \n");
   printf("  -s                                                                           \n"); 
-  printf("  --sufix             - should I create suffix files or not?                   \n");
+  printf("  --sufix             - Should I create suffix files or not?                   \n");
   printf("                        If you want me to create suffix files                  \n");
-  printf("                        I will create each new file after limit                \n");
+  printf("                        I will create each new suffix file after limit         \n");
   printf("                        is reached. Otherwise, I will overwrite                \n");
   printf("                        oryginal log. I mean, I will overwrite it              \n");
   printf("                        like destroying it, cleaning, you will get             \n");
   printf("                        no nothing.                                            \n");
+  printf("                                                                               \n");
   printf("  -l limit                                                                     \n");
-  printf("  --limit=limit       - log size limit; default is 1GB. Unfortunately          \n");
+  printf("  --limit=limit       - Log size limit; default is 1GB. Unfortunately          \n");
   printf("                        you have to specify full size, like 1000000000 for 1GB \n");
+  printf("                                                                               \n");
+  printf("  -r                                                                           \n");
+  printf("  --rollover          - If you set roll over flag, I will not destroy content  \n");
+  printf("                        However, tail -F will not work in this case. So, you   \n");
+  printf("                        are the one to decide which log style you prefer       \n");
+  printf("                                                                               \n");
   printf("  -h                                                                           \n");
   printf("  --help              - surprize, surprize! I will show you help message.      \n");
   exit(1);
@@ -57,12 +68,13 @@ void get_options(int argc, char **argv) {
       { "suffix", no_argument, 0, 's' },
       { "limit",  required_argument, 0, 'l' },
       { "file",  required_argument, 0, 'f'},
+      { "rollover", no_argument, 0, 'r' },
       { "help", no_argument, 0, 'h' },
       { 0, 0, 0, 0}
     };
     int option_index = 0;
 
-    c = getopt_long (argc, argv, "sl:f:h?",
+    c = getopt_long (argc, argv, "srl:f:h?",
                      long_options, &option_index);
 
     /* Detect the end of the options. */
@@ -92,6 +104,10 @@ void get_options(int argc, char **argv) {
         display_usage();
         break;
 
+      case 'r':
+        roll_over = 1;
+        break;
+
       default:
         break;
     }
@@ -99,12 +115,14 @@ void get_options(int argc, char **argv) {
 }
 
 int main(int argc, char **argv) {
-  FILE *fp;
-  FILE *fp_suffix = NULL;
-  unsigned long long pos = 0;
-  char *suffix = NULL;
-  char curr_suffix_first = 'a';
-  char curr_suffix_second = 'a';
+
+  FILE *fp                     = NULL;  // file pointer for log file
+  FILE *fp_suffix              = NULL;  // file pointer for suffix files
+  unsigned long long pos       = 0;     // current count of bytes transferred
+  char *suffix                 = NULL;  // name of suffix file
+  char curr_suffix_first       = 'a';   // current index at first digit
+  char curr_suffix_second      = 'a';   // current index at second digit
+  int c                        = 0;     // character read from stdin
 
   get_options(argc, argv); 
 
@@ -117,17 +135,25 @@ int main(int argc, char **argv) {
     exit(1);
   }
 
-  // we need length of file name
-  // + dot
-  // + aa-zz
-  // + '\0'
-  suffix = malloc(strlen(file_name) + 4);
+  // open log file specified by user (it will be destroyed)
   if((fp = fopen(file_name, "w")) == NULL) {
     fprintf(stderr, "I can't open output file: %s\n", file_name);
     exit(1);
   }
 
+  // in case user wants to use suffix files, we have to
+  // allocate memory for the name and open initial
+  // suffix file
   if( use_suffix ) {
+    // we need length of file name
+    // + dot
+    // + aa-zz
+    // + '\0'
+    if((suffix = malloc(strlen(file_name) + 4)) == NULL) {
+      fprintf(stderr, "Fatal error! Failed to allocate memory\n");
+      exit(1);
+    }
+
     suffix = suffix_file_name(suffix, file_name, curr_suffix_first, curr_suffix_second);
     if((fp_suffix = fopen(suffix, "w")) == NULL) {
       fprintf(stderr, "I can't open suffix file: %s\n", suffix);
@@ -136,7 +162,6 @@ int main(int argc, char **argv) {
     }
   }
 
-  int c;
   while((c = getc(stdin)) != EOF) {
     // instead of copying file to another location (after limit is reached)
     // I write into oryginal log and suffix log at the same time
@@ -166,6 +191,17 @@ int main(int argc, char **argv) {
       // close file
       fclose(fp);
       // reopen file
+      // if we want to roll over, we have to fseek
+      // after reopening file in r+ mode
+      // otherwise, we can discard the content
+      if( roll_over == 0 ) {
+        fp = fopen(file_name, "w");
+      } else {
+        fp = fopen(file_name, "r+");
+        if( fp != NULL ) {
+          fseek(fp, 0, 0);
+        }
+      } 
       if((fp = fopen(file_name, "w")) == NULL) {
         fprintf(stderr, "I can't open output file: %s\n", file_name);
         exit(1);
@@ -195,11 +231,13 @@ int main(int argc, char **argv) {
         }
       }
 
+      // reset current counter
+      // we will start over from the begging until we reach limit
       pos = 0;
 
     }
   }
  
-  fclose(fp);
-  fclose(fp_suffix);
+  if(fp != NULL) fclose(fp);
+  if(fp_suffix != NULL) fclose(fp_suffix);
 }
